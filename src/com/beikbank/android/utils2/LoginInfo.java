e2b0893d21b5da44caa5e88ab1a77c5c17103e44
ygh@beikbank.com <b5722333>
2018-01-03 10:51:33 +0800

    Rejected commit: Default

---
+++ src/com/beikbank/android/utils2/LoginInfo.java
@@ -0,0 +1,95 @@
+package com.beikbank.android.utils2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+
+import com.beikbank.android.exception.LogHandler;
+import com.beikbank.android.utils.HuanjingInfo;
+import com.beikbank.android.utils.Utils;
+import com.umeng.message.UmengRegistrar;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * Created by Administrator on 2018/1/2.
+ */
+
+public class LoginInfo
+{
+
+    public static String getIPAddress(Context context) {
+        NetworkInfo info = ((ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if (info != null && info.isConnected()) {
+            if (info.getType() == ConnectivityManager.TYPE_MOBILE) {//当前使用2G/3G/4G网络
+                try {
+                    //Enumeration<NetworkInterface> en=NetworkInterface.getNetworkInterfaces();
+                    for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                        NetworkInterface intf = en.nextElement();
+                        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+
+                            InetAddress inetAddress = enumIpAddr.nextElement();
+                            if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                                return inetAddress.getHostAddress();
+                            }
+                        }
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+
+            } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {//当前使用无线网络
+                WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());//得到IPV4地址
+                return ipAddress;
+            }
+        } else {
+            //当前无网络连接,请在设置中打开网络
+        }
+        return null;
+    }
+    /**
+     * 将得到的int类型的IP转换为String类型
+     *
+     * @param ip
+     * @return
+     */
+    private static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
+    }
+    public static HuanjingInfo getHJ(Activity act)
+    {
+        HuanjingInfo hj=new HuanjingInfo();
+        hj.osVersion= Build.VERSION.RELEASE+":"+Build.VERSION.SDK;
+        hj.softVersion= Utils.getVersion(act);
+
+        String device_model = Build.MODEL;
+        if(device_model!=null)
+        {
+            hj.rasModel=device_model;
+        }
+
+        //检查参数
+        if(hj.osVersion==null||hj.rasModel==null||hj.rasType==null||hj.softVersion==null||hj.umDeviceToken==null)
+        {
+            hj=new HuanjingInfo();
+        }
+        return hj;
+    }
+
+
+
+}
